4a0df2a032f9723b4c171a497325834f2d1d3ecd
luis miguel gonzalez <luismg8812@hotmail.com>
2019-03-29 11:17:09 -0500

    'first'

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebaseConfig:{
     apiKey: "AIzaSyAC-WfbtrYHUo42QOubSf9NdM73rWqOqFo",
     authDomain: "facturacion-cloud.firebaseapp.com",

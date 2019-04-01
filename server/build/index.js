1d65fa0787806c84ae10eae21bac995d52b37197
luis miguel gonzalez <luismg8812@hotmail.com>
2019-03-31 19:38:45 -0500

    no message

--- server/build/index.js
+++ server/build/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("server");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+class Server {
+    constructor() {
+        express_1.default();
+    }
+}
+new Server();
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,10 @@
-console.log("server");
+import express, {Application} from 'express';
+
+class Server{
+   public app:Application;
+
+    constructor(){
+        express();
+    }
+}
+new Server();
